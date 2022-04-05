063e88724e827b382c029c80b41289974d6f274b
Bogsana <bogomil_cvetkov@abv.bg>
2022-04-05 19:51:14 +0300

    Added a clean-up job, that closes all remaining browser sessions

---
+++ Project/model/all_models.py
@@ -0,0 +1,9 @@
+# Importing all models from here because of circular imports issue
+
+from Project.model.account_model import AccountModel
+from Project.model.user_model import UserModel
+from Project.model.page_model import PageModel
+from Project.model.logs_model import LogModel
+from Project.model.reset_pass_model import ResetPassModel
+from Project.model.screenshot_model import ScreenShotModel
+from Project.model.schedule_model import ScheduleModel
